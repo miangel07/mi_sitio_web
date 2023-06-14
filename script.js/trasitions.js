36bbc24262fa4aad286f57ff8ee4d1fd639af3bb
miangel07 <miguelosoriorojas063@gmail.com>
2023-06-13 23:49:57 -0500

    add file

--- script.js/trasitions.js
+++ script.js/trasitions.js
@@ -1,19 +1,29 @@
 
 let contenedorGrande= document.getElementById("contenedorGrande");
 let contenedor =document.getElementById("contenedor");
+let noche =document.getElementById("noche");
+let texto =document.getElementById("textoClaro");
 
 contenedorGrande.addEventListener("click",function(){
 if(contenedor.classList.contains("contenedor-1")){
 contenedor.classList.add("contenedor-2");
 contenedor.classList.remove("contenedor-1");
-contenedor.classList.add("main-box-off");
-contenedor.classList.remove("main-box-on");
+contenedorGrande.classList.add("main-box-on");
+contenedorGrande.classList.remove("main-box-off");
+noche.classList.add("noche");
+noche.classList.remove("dia")
+texto.classList.add("p");
+texto.classList.remove("oscuro");
 }
 else{
     contenedor.classList.remove("contenedor-2")
     contenedor.classList.add("contenedor-1")
     contenedorGrande.classList.remove("main-box-on");
     contenedorGrande.classList.add("main-box-off");
+    noche.classList.remove("noche");
+    noche.classList.add("dia");
+    texto.classList.remove("p");
+    texto.classList.add("oscuro");
 }
 })
 
