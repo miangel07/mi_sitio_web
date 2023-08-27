bb4f5b88ae717af7120177144589d19ff5005ef4
miangel07 <miguelosoriorojas063@gmail.com>
2023-08-26 20:25:56 -0500

    sks

--- script.js/starbucks.js
+++ script.js/starbucks.js
@@ -2,20 +2,20 @@ let iceGreen = document.getElementById("iceGreen");
 let iceRose = document.getElementById("iceRose");
 let icePink = document.getElementById("icePink");
 let mainImg = document.getElementById("mainImg");
-iceGreen.addEventListener("mouseover", function(){
+iceGreen.addEventListener("click", function(){
     mainCircle.classList.add("main-circle-green");
     mainCircle.classList.remove("main-circle-rose");
     mainCircle.classList.remove("main-circle-pink");
     mainImg.setAttribute("src","/img/verde.png");
 }
 )
-iceRose.addEventListener("mouseover", function(){
+iceRose.addEventListener("click", function(){
     mainCircle.classList.remove("main-circle-green");
     mainCircle.classList.add("main-circle-rose");
     mainCircle.classList.remove("main-circle-pink");
     mainImg.setAttribute("src","/img/rosa.png");
 })
-icePink.addEventListener("mouseover", function(){
+icePink.addEventListener("click", function(){
     mainCircle.classList.remove("main-circle-green");
     mainCircle.classList.remove("main-circle-rose");
     mainCircle.classList.add("main-circle-pink");
