7219fff7e3c810267784021e34556ddbed60cbce
victor-losada <victorlosada2211@gmail.com>
2023-10-17 14:47:50 -0500

    add file

--- script.js/carrito.js
+++ script.js/carrito.js
@@ -1,16 +1,45 @@
 let boxPlus=document.getElementById("boxPlus");
 let boxMinus=document.getElementById("boxMinus");
 let boxUnidades=document.getElementById("boxUnidades");
+let subTotal=document.getElementById("subTotal");
+const valorUnidad=85000;
+const valorUnidadStr=Intl.NumberFormat("de-De").format(valorUnidad);
+document.getElementById("precioUnidad").innerHTML="$"+ valorUnidadStr;
+
 boxPlus.addEventListener("click",function(){
     let auxUnidades= parseInt(boxUnidades.innerHTML)
     if(auxUnidades <=9){
-        document.getElementById("boxUnidades").innerHTML=(auxUnidades + 1 ).toString()
-    }
+       boxUnidades.innerHTML=(auxUnidades + 1).toString();
+       let valorSubtotal= parseInt(boxUnidades.innerHTML)*valorUnidad;
+       subTotal.innerHTML=Intl.NumberFormat("de-DE").format
+       (valorSubtotal);
+       console.log(parseInt(boxUnidades.innerHTML)* parseInt(valorUnidad));
+
+       if(boxUnidades.innerHTML == 1){
+        document.getElementById("cantProductos").
+        innerHTML=boxUnidades.innerHTML +"Producto";
+       }
+       else if(boxUnidades.innerHTML >= 2){
+        document.getElementById("cantProductos").innerHTML= boxUnidades.innerHTML + " Productos";
+       }
+    };
     
 });
 boxMinus.addEventListener("click",function(){
 let auxUnidades2=parseInt(boxUnidades.innerHTML)
 if(auxUnidades2 >0){
     document.getElementById("boxUnidades").innerHTML=(auxUnidades2 - 1).toString()
+    let valorSubtotal= parseInt(boxUnidades.innerHTML)*valorUnidad;
+       subTotal.innerHTML=Intl.NumberFormat("de-DE").format
+       (valorSubtotal);
+       console.log(parseInt(boxUnidades.innerHTML)* parseInt(valorUnidad));
+       
+       if(boxUnidades.innerHTML == 1){
+        document.getElementById("cantProductos").
+        innerHTML=boxUnidades.innerHTML +"Producto";
+       }
+       else{
+        document.getElementById("cantProductos").innerHTML= boxUnidades.innerHTML + " Productos";
+       }
 }
 });
