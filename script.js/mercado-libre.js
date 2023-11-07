6b71e5af55247ac9e7e3425e0ac08f8ad6442a5b
victor-losada <victorlosada2211@gmail.com>
2023-11-07 16:41:20 -0500

    add file

--- script.js/mercado-libre.js
+++ script.js/mercado-libre.js
@@ -1,15 +1,44 @@
-let mainContent= document.getElementById("mainContent");
+let boxProductos= document.getElementById("boxProductos")
 
-let boxProducto= document.createElement("div");
-mainContent.appendChild(boxProducto);
+let boxProducto= document.createElement("div")
 
-boxProducto.setAttribute("class","marca-producto");
-let marcaProducto = document.createElement("label");
-mainContent.appendChild(marcaProducto);
+boxProductos.appendChild(boxProducto);
+boxProducto.setAttribute("class","box-producto")
 
-let nodoMarcaProducto=document.createTextNode("mazda");
-marcaProducto.appendChild(nodoMarcaProducto);
+let boxImagen = document.createElement("div")
+boxProducto.appendChild(boxImagen)
+boxImagen.setAttribute("class","box-imagen")
 
-let imgProducto=document.createElement("img");
-mainContent.appendChild(imgProducto)
-imgProducto.setAttribute("src","img/car.webp");
+let imagenProducto=document.createElement("img")
+boxImagen.appendChild(imagenProducto)
+imagenProducto.setAttribute("src","img/car.webp")
+imagenProducto.setAttribute("class","imagen-producto")
+
+let infoProducto=document.createElement("div")
+boxProducto.appendChild(infoProducto)
+infoProducto.setAttribute("class","box-info-producto")
+
+let marcaProducto= document.createElement("label")
+infoProducto.appendChild(marcaProducto)
+let nodoMarcaProducto=document.createTextNode("mazda")
+marcaProducto.appendChild(nodoMarcaProducto)
+
+let modeloProducto= document.createElement("label")
+infoProducto.appendChild(modeloProducto)
+let nodoMarcaProducto2=document.createTextNode(" mazda modelo cx-5 ")
+modeloProducto.appendChild(nodoMarcaProducto2)
+
+
+let precioProducto= document.createElement("label")
+let nodoMarcaProducto3=document.createTextNode("$50.000.000")
+infoProducto.appendChild(nodoMarcaProducto3);
+
+let yearProducto=document.createElement("label")
+infoProducto.appendChild(yearProducto);
+let yearProducto1=document.createTextNode("año 2020")
+yearProducto.appendChild(yearProducto1)
+
+let kilometro=document.createElement("label");
+infoProducto.appendChild(kilometro)
+let kilometrostxt=document.createTextNode("20.000")
+kilometro.appendChild(kilometrostxt)
