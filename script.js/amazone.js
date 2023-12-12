cbee3b7e66ada8a27da51695e9a3eca0f727ff27
miangel07 <miguelosoriorojas063@gmail.com>
2023-12-12 16:40:28 -0500

    add file

--- script.js/amazone.js
+++ script.js/amazone.js
@@ -3,6 +3,11 @@ let puntaje4 = document.getElementById('puntaje4');
 let puntaje3 = document.getElementById('puntaje3');
 let puntaje2 = document.getElementById('puntaje2');
 let puntaje1 = document.getElementById('puntaje1');
+let porcentajeCalificacion1= document.getElementById('porcentajeCalificacion1');
+let porcentajeCalificacion2= document.getElementById('porcentajeCalificacion2');
+let porcentajeCalificacion3= document.getElementById('porcentajeCalificacion3');
+let porcentajeCalificacion4= document.getElementById('porcentajeCalificacion4');
+let porcentajeCalificacion5= document.getElementById('porcentajeCalificacion5');
 
 let btnGraficar = document.getElementById('Graficar')
 
@@ -20,6 +25,14 @@ btnGraficar.addEventListener('click', function(){
    let  por3 = (val3*100)/sumaTotal;
    let  por2 = (val2*100)/sumaTotal;
    let  por1 = (val1*100)/sumaTotal;
+  console.log(por5.toFixed(0));
+  porcentajeCalificacion1.style.width=(por1 * 2)+"px";
+  porcentajeCalificacion2.style.width=(por2 * 2)+"px";
+  porcentajeCalificacion3.style.width=(por3 * 2)+"px";
+  porcentajeCalificacion4.style.width=(por4 * 2)+"px";
+  porcentajeCalificacion5.style.width=(por5 * 2)+"px";
 
-    alert(por1);
+  let spanPorsentaje=document.createElement('span');
+  
+  
 });
